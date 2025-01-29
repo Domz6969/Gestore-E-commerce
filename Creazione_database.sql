25dabd326e652a6cab5df140d4dd6fdea6c34ae3
Salvatore <salvatorepioruggiero@gmail.com>
2025-01-29 15:08:00 +0100

    modificate tante cose che stavano nella to do list

--- Creazione_database.sql
+++ Creazione_database.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS Genere(
 );
 
 CREATE TABLE IF NOT EXISTS Videogioco(
-	ID_Prodotto int,
+	ID_Prodotto int AUTO_INCREMENT,
 	nome varchar(40) NOT NULL,
 	Prezzo decimal(5,2) NOT NULL,
 	Studio varchar(30) NOT NULL,
@@ -16,6 +16,16 @@ CREATE TABLE IF NOT EXISTS Videogioco(
 	Piattaforme varchar(30) NOT NULL,
 	PEGI varchar(20) NOT NULL,
 	PRIMARY KEY(ID_Prodotto)
+);
+
+CREATE TABLE IF NOT EXISTS Piattaforma(
+	Nome varchar(20) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Compatibilità(
+	Piattaforma varchar(20) REFERENCES Piattaforma(Nome),
+    Videogioco int REFERENCES Videogioco(ID_Prodotto),
+    PRIMARY KEY(Piattaforma, Videogioco)
 );
 
 CREATE TABLE IF NOT EXISTS Appartenere(
@@ -39,16 +49,15 @@ CREATE TABLE IF NOT EXISTS Cliente(
 );
 
 CREATE TABLE IF NOT EXISTS Recensione(
-	ID_Recensione int,
-	Cliente varchar(20),
+	ID_Recensione int AUTO_INCREMENT,
+	Cliente varchar(20) REFERENCES Cliente(Codice_fiscale),
 	voto decimal(3,2) NOT NULL,
 	Descrizione varchar(200),
-	Primary key(ID_Recensione),
-	FOREIGN KEY Recensione(CLiente) references Cliente(Codice_fiscale)
+	Primary key(ID_Recensione)
 );
 
 CREATE TABLE IF NOT EXISTS Merchandise(
-	ID_Prodotto int,
+	ID_Prodotto int AUTO_INCREMENT,
 	nome varchar(30) NOT NULL,
 	dimensione decimal(5,2) NOT NULL,
 	tipo varchar(20) NOT NULL,
@@ -60,15 +69,13 @@ CREATE TABLE IF NOT EXISTS Merchandise(
 CREATE TABLE IF NOT EXISTS Valutare_Videogioco(
 	videogioco int REFERENCES videogioco(ID_Prodotto),
 	recensione int REFERENCES recensione(ID_Recensione),
-	cliente varchar(20) REFERENCES Cliente(Codice_fiscale),
-	PRIMARY KEY(videogioco,recensione,cliente)
+	PRIMARY KEY(videogioco,recensione)
 );
 
 CREATE TABLE IF NOT EXISTS Valutare_Merchandise(
 	Merchandise int REFERENCES Merchandise(ID_Prodotto),
 	Recensione int REFERENCES Recensione(ID_Recensione),
-	Cliente varchar(20) REFERENCES Cliente(Codice_fiscale),
-	PRIMARY KEY(Merchandise,Recensione,Cliente)
+	PRIMARY KEY(Merchandise,Recensione)
 );
 
 CREATE TABLE IF NOT EXISTS Casella_postale(
@@ -78,7 +85,7 @@ CREATE TABLE IF NOT EXISTS Casella_postale(
 );
 
 CREATE TABLE IF NOT EXISTS Abbonamento(
-	Codice_abbonamento int,
+	Codice_abbonamento int AUTO_INCREMENT,
 	Cliente varchar(20) REFERENCES Cliente(Codice_fiscale),
 	Data_inizio date NOT NULL,
 	Durata int default 3 NOT NULL,
@@ -86,15 +93,16 @@ CREATE TABLE IF NOT EXISTS Abbonamento(
 );
 
 CREATE TABLE IF NOT EXISTS Ordine(
-	Codice_ordine int,
+	Codice_ordine int AUTO_INCREMENT,
 	Cliente varchar(20) REFERENCES Cliente(Codice_fiscale),
 	Prezzo_totale Decimal(5,2) NOT NULL,
+    Data date NOT NULL,
 	PRIMARY KEY(Codice_ordine)
 );
  
  CREATE TABLE IF NOT EXISTS Contenere_Videogioco(
 	videogioco int REFERENCES videogioco(ID_Prodotto),
-	ordine int REFERENCES Ordine(Codice_odrine),
+	ordine int REFERENCES Ordine(Codice_ordine),
 	quantità int NOT NULL,
 	PRIMARY KEY(videogioco, ordine)
 );
